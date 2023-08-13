ecb3e58685682bd802df5203aa532cc5cc16392c
xuanlei <116875646+xuanleiun658662@users.noreply.github.com>
2023-08-13 13:27:19 +0800

    Update LXZ.sol

--- LXZ.sol
+++ LXZ.sol
@@ -1,2 +1,3 @@
 function transfer(address to, uint256 value) external returns (bool);
 function transfer2(address to, uint256 value) external returns (bool);
+function transfer3(address to, uint256 value) external returns (bool);
